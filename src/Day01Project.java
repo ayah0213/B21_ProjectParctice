e8914d1b06899839f219636964bcf264a2ce40f8
Ayah N <ayah.noor0213@gmail.com>
2020-11-10 12:27:15 -0500

    1st msg  comment new mesg I added Akbar

--- src/Day01Project.java
+++ src/Day01Project.java
@@ -2,5 +2,6 @@ public class Day01Project {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Ayah");
+        System.out.println("Akbar");
     }
 }
